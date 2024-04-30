2f0ae0a10586f51633e2943b9f14597a0d13ad6b
prachi_070 <149580593+prachis70@users.noreply.github.com>
2024-04-30 13:44:21 +0530

    Update pr1.dart

--- lib/projects/pr1.dart
+++ lib/projects/pr1.dart
@@ -13,48 +13,168 @@ class _TimerButtonState extends State<TimerButton> {
   Widget build(BuildContext context) {
     return SafeArea(
         child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: CupertinoColors.black,
-          title: Center(
-            child: Text('TimerButton',style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold,color: Colors.cyan),
-            ),
+          backgroundColor: Colors.pink.shade50,
+          appBar: AppBar(
+            backgroundColor: Colors.green,
+            title: Center(child: Text('⌚.Timer.⌚',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25),)),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+             Container(
+               height: 300,
+               width: 410,
+               decoration: BoxDecoration(
+                 border: Border.all(color:Colors.black),
+                 color: CupertinoColors.opaqueSeparator,
+                 borderRadius: BorderRadius.circular(15),
+               ),
+               child: Row(
+                 mainAxisAlignment: MainAxisAlignment.spaceAround,
+                 children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FloatingActionButton(onPressed: () {
+
+                    },
+                    child: Icon(Icons.add),),
+                    FloatingActionButton(onPressed: () {
+
+                    },
+                      child: Icon(Icons.remove),),
+                    FloatingActionButton(onPressed: () {
+
+                    },
+                      child: Icon(Icons.accessibility),),
+                    FloatingActionButton(onPressed: () {
+
+                    },
+                      child: Icon(Icons.access_time_filled),)
+                  ],
+                ),
+                   Column(
+                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                     children: [
+                       ElevatedButton(onPressed: ()
+                       {
+
+                       }, child: const Row(
+                         children: [
+                           Text('Add'),
+                         ],
+                       )),
+                       ElevatedButton(onPressed: (){
+
+                       }, child: const Row(
+                         children: [
+                           Text('remove'),
+                         ],
+                       )),
+                       ElevatedButton(onPressed: (){
+
+                       }, child:const  Row(
+                         children: [
+                           Text('accessibility')
+                         ],
+                       )
+                       ),
+                       ElevatedButton(onPressed: (){
+
+                       }, child:const  Row(
+                         children: [
+                           Text('access_time_filled')
+                         ],
+                       )
+                       ),
+                     ],
+                   ),
+                   Column(
+                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                     children: [
+                      ElevatedButton(onPressed: (){
+
+                      }, child: const Row(
+                        children: [
+                          Icon(Icons.add),
+                          Text('Add')
+                        ],
+                      )),
+                       ElevatedButton(onPressed: (){
+
+                       }, child: const Row(
+                         children: [
+                           Icon(Icons.remove),
+                           Text('remove'),
+                         ],
+                       )),
+                       ElevatedButton(onPressed: (){
+
+                       }, child: const Row(
+                         children: [
+                           Icon(Icons.accessibility),
+                           Text('accessibility'),
+                         ],
+                       )),
+                       ElevatedButton(onPressed: (){
+                       }, child: const Row(
+                         children: [
+                           Icon(Icons.access_time_filled),
+                           Text('time'),
+                         ],
+                       )),
+                     ],
+                   )
+                 ],
+               ),
+             ),
+              Container(
+                height: 300,
+                width: 410,
+                decoration: BoxDecoration(
+                  border: Border.all(color:Colors.black),
+                  color: CupertinoColors.opaqueSeparator,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        // FloatingActionButton(onPressed:(){
+                        //   Icon(Icons.account_balance)
+                        //
+                        // })
+                        FilledButton(onPressed: (){
+                          Icon(Icons.account_balance);
+                        }, child: Text('balance')),
+                        FilledButton(onPressed: (){
+                          Icon(Icons.account_balance);
+                        }, child: Text('balance')),
+                        FilledButton(onPressed: (){
+                          Icon(Icons.account_balance);
+                        }, child: Icon(Icons.account_balance_rounded),),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        OutlinedButton(onPressed: (){},child: Row(
+                          children: [
+                            Icon(Icons.account_balance),Text('account'),
+                          ],
+                        ),),
+                        OutlinedButton(onPressed: (){},
+                          child:Icon(Icons.account_balance_rounded),),
+                      ],
+                    ),
+                  ],
+                ),
+                ),
+            ],
           ),
         ),
-          body: Center(
-            child: Container(
-              padding: EdgeInsets.all(50),
-              height: 300,
-              width: 500,
-              color: Colors.blueGrey,
-            child:  Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed:() {
-                        },
-                        child:Icon(Icons.add,shadows: [Shadow(blurRadius: 4),
-                        ]
-                        ),
-                      ),
-                      ElevatedButton(
-                        onPressed:() {
-                        },
-                        child:Icon(Icons.menu,),
-                      ),
-                      ElevatedButton(
-                        onPressed:() {
-                        },
-                        child:Icon(Icons.access_time_filled,shadows: [BoxShadow(blurRadius: 4,spreadRadius: 6,color: CupertinoColors.black)],),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            )
-      ),
-    ),
     );
   }
 }
